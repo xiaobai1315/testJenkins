222858d91727fdeab5dbb0f2b794d1d2450bf3bf
张洪杰 <zhanghongjie11@ZBMAC-C02D5236M.local>
2021-05-20 18:11:04 +0800

    11

--- src/main/java/com/study/TestController.java
+++ src/main/java/com/study/TestController.java
@@ -1,8 +1,9 @@
 package com.study;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 public class TestController {
 
