6ae7aa445933a84d0c33b0c398a7455418d26b9c
xiaobai1315 <xiaobai1315@yeah.net>
2022-01-22 08:21:17 +0800

    Update TestController.java

--- src/main/java/com/study/TestController.java
+++ src/main/java/com/study/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @RequestMapping(path = "test")
     public String test() {
-        return "this is test Jenkins";
+        return "this is test Jenkins 111";
     }
 }
